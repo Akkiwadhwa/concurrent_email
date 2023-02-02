968ad8dd83e56245c49df472e4ac7caa0ad50c14
Akkiwadhwa <114605480+Akkiwadhwa@users.noreply.github.com>
2023-02-03 01:11:49 +0530

    2

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import ast
 import concurrent.futures
 import os
+from threading import Lock
 
-import pandas as pd
 import requests
 from fake_useragent import UserAgent
 from requests.structures import CaseInsensitiveDict
+
+prx = open('proxies.txt', 'r')
+prx1 = prx.readlines()
+http_prx = prx1[0].split('>')[-1].strip()
+https_prx = prx1[1].split('>')[-1].strip()
+
+com = open("sent.txt", "a")
+failed = open("failed.txt", "a")
+com1 = open("sent.txt", "r")
+failed1 = open("failed.txt", "r")
+q = open("total.txt", "a")
+new = []
+li1 = []
+sending = []
+failed_mail = []
+lock = Lock()
+sent = com1.readlines()
+for i in sent:
+    a = ast.literal_eval(i.replace("\n", ""))
+    new.append(a)
 
 
 def mail(di):
@@ -23,25 +44,22 @@ def mail(di):
     url = "https://api.smtprelay.co/contact/add?version=2"
 
     for key, value in di.items():
+        print(di)
         data = f"email={value}&submit=Subscribe&publicaccountid={key}&returnUrl=https%253A%252F" \
                "%252Fgoogle.com&activationReturnUrl=&alreadyactiveurl=&activationTemplate=newstandard&source=WebForm" \
                "&verifyemail=false&captcha=false&sendActivation=true&notifyEmail= "
-        prx = open('proxies.txt', 'r')
-        prx1 = prx.readlines()
-        http_prx = prx1[0].split('>')[-1].strip()
-        https_prx = prx1[1].split('>')[-1].strip()
+
         proxies = {"http": http_prx, "https": https_prx}
-        print("a")
-        resp = requests.post(url, headers=headers, data=data)
-        failed_mail.append(di)
-        prx.close()
+
+        resp = requests.post(url, headers=headers, data=data, proxies=proxies)
+        if resp.status_code == 200:
+            with lock:
+                com.write(str(di) + "\n")
 
         if resp.status_code != 200:
-            print("b")
+            failed.write(str(di) + "\n")
 
 
-li1 = []
-failed_mail = []
 o = input("Press a for sending emails,b for sending failed.csv emails: ").capitalize()
 if o == "A":
     files = os.listdir('email')
@@ -63,14 +81,14 @@ if o == "A":
                         for i in email:
                             a = {p1.strip(): i.strip()}
                             li1.append(a)
+    for i in li1:
+        q.write(str(i) + "\n")
+    sending = [i for i in li1 if i not in new]
+    print(f"total emails to sent -->  {len(sending)}")
+
 if o == "B":
-    a = open("failed.csv").readlines()
+    sending = failed1.readlines()
+    print(f"total emails to sent --> {len(sending)}")
 
-
-print(a)
-# print(f"total emails to sent -->  {len(li1)}")
-# with concurrent.futures.ThreadPoolExecutor(16) as executor:
-#     executor.map(mail, li1)
-#
-# df = pd.DataFrame(failed_mail)
-# df.to_csv("failed.csv")
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(mail, sending)
